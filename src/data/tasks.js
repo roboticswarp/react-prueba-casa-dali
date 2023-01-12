0da9cb4bac603c82f711f3b032804c4c90338d20
roboticswarp <roboticswarpdesarrollador@gmail.com>
2023-01-12 13:21:40 -0500

    prueba 1 casa-dali

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarrea",
+    description: "description de mi primer tarrea",
+  },
+  {
+    id: 1,
+    title: "mi second tarrea",
+    description: "description de mi second tarrea",
+  },
+  {
+    id: 2,
+    title: "mi ter tarrea",
+    description: "description de mi ter tarrea",
+  },
+];
